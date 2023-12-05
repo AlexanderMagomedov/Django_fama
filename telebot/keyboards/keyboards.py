46578936c9c37522ee254ed0be33fd165bb47299
ÐAlexander Magomedov <sistem4ik@mail.ru>
2023-12-05 22:42:28 +0500

    add story list in inlainbutton. SQL request

--- telebot/keyboards/keyboards.py
+++ telebot/keyboards/keyboards.py
@@ -1,7 +1,6 @@
+import sqlite3
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from telebot.lexicon.book_name import book_name
-
 from telebot.lexicon.lexicon_ru import LEXICON_RU
 
 
@@ -18,12 +17,16 @@ def create_back_keyboard(arg) -> InlineKeyboardMarkup:
 
 # Функция создания инлайн кнопкок со списком сказок
 def create_story_list_keyboard() -> InlineKeyboardMarkup:
+    connect = sqlite3.connect('db.sqlite3')
+    cursor = connect.cursor()
+    rezult = (list(map(lambda x: x[0], cursor.execute("SELECT book_name FROM telebot_book").fetchall())))
+    connect.close()
     # Создаем объект клавиатуры
     kb_builder = InlineKeyboardBuilder()
     # Наполняем клавиатуру кнопками-закладками
     kb_builder.row(*[InlineKeyboardButton(
         text=story_name,
-        callback_data=story_name) for story_name in book_name.keys()], width=1)
+        callback_data=story_name) for story_name in rezult], width=1)
     kb_builder.row(InlineKeyboardButton(
         text=LEXICON_RU['back'],
         callback_data='/start'))
